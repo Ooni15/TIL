c9cb1b1b4ea9dbfcdc8a22692501b888828af301
Ooni15 <audfkddns@naver.com>
2024-10-14 16:03:41 +0900

    1011_BOJ_8911_fail

---
+++ ALGORITHM/BEAKJOON/BOJ_8911/8911_sol1.py
@@ -0,0 +1,19 @@
+def turtle(lst,n) :
+
+    X, Y = [], []
+    for i in range(n):
+        now = lst[i]
+
+        if now == 'F' :
+            pass
+
+
+
+T = int(input())
+
+for test_case in range(T) :
+    arr = list(input())
+
+    N = len(arr)
+    turtle(arr, N)
+
