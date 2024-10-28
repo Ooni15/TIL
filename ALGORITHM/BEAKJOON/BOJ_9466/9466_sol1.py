e633ed9ba7610168e65184fb399f061b4bb1960e
Rangun <audfkddns@naver.com>
2024-10-29 08:43:33 +0900

    1028_BOJ_9466_fail

---
+++ ALGORITHM/BEAKJOON/BOJ_9466/9466_sol1.py
@@ -0,0 +1,16 @@
+T = int(input())
+
+def dfs(start):
+    
+
+
+
+
+
+for test_case in range(T):
+    n = int(input())
+    graph = [0] + list(map(int,input().split()))
+    print(graph)
+
+    # 1부터 돌아
+    # 하나의 사이클이 나오면 ㅈㅈ
